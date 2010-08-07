6f24283f2ac3f8a2a33f8cb2572293a176a462bc
Nathan Hoad <nathan@nathanhoad.net>
2010-08-07 22:07:29 +0800

    Fixed fuzzy dates

--- NSDate_Fuzzy.m
+++ NSDate_Fuzzy.m
@@ -20,19 +20,19 @@
 		return @"less than a minute ago";
 	}
 	if (t < 50 * MINUTE) {
-		return [NSString stringWithFormat:@"%d minutes ago", round(t / MINUTE)];
+		return [NSString stringWithFormat:@"%.0f minutes ago", round(t / MINUTE)];
 	}
 	if (t < 90 * MINUTE) {
 		return @"about an hour ago";
 	}
 	if (t < 18 * HOUR) {
-		return [NSString stringWithFormat:@"%d hours ago", round(t / HOUR)];
+		return [NSString stringWithFormat:@"%.0f hours ago", round(t / HOUR)];
 	}
 	if (t < 2 * DAY) {
 		return @"about a day";
 	}
 	if (t < 20 * DAY) {
-		return [NSString stringWithFormat:@"%d days ago", round(t / DAY)];
+		return [NSString stringWithFormat:@"%.0f days ago", round(t / DAY)];
 	}
 	if (t < 1 * MONTH) {
 		return @"about a month ago";
